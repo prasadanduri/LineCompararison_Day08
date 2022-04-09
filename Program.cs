b58299851a5367cb671de2023b4bd9038a91e1fa
prasad <prasadanduri13gmail.com>
2022-04-09 16:50:37 +0530

    check line length is greater/less

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Linecomp
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("... Welcome to Line Comparison Computation ... ");
+
+            checkequlityline Linecom = new checkequlityline();
+            Linecom.length_line();
+            Linecom.Comparelines();
+            Console.ReadKey();
+
+        }
+    }
+}
